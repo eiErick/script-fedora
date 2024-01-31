1e86ea36d7278c44556707e28b3120f0615e405a
Érick <eierickdasilva@gmail.com>
2024-01-31 14:37:34 -0300

    Update makeBackup.sh

--- fedora/makeBackup.sh
+++ fedora/makeBackup.sh
@@ -3,26 +3,28 @@ paste="./data"
 
 if [ -d "$paste" ]; then
 	echo "ATENÇÃO: A pasta $paste já existe neste diretório, deseja apagar? [s/n]"
-	
-	read response 
-	if [ "$response" == "s" ]; then 
+
+	read response
+	if [ "$response" == "s" ]; then
 		echo ""
 		echo "Reescrevendo..."
-	else 
+	else
 		echo ""
 		echo "Operação cancelada!"
 		exit
 	fi
-		
+
 else
 	mkdir $paste
 fi
 
 touch $paste/flatpak.txt
 touch $paste/snap.txt
+touch $paste/extensions.txt
 
 flatpak list --app --columns=application > $paste/flatpak.txt
 snap list | awk '{print $1}' | tail -n +2 > $paste/snap.txt
+gnome-extensions list > $paste/extensions.txt
 
 cp ~/.bash_aliases $paste
 mv $paste/.bash_aliases $paste/aliases.txt
