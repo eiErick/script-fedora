4267fc3a296130cb76527e158eb553a511b1949e
Érick <eierickdasilva@gmail.com>
2024-02-25 08:07:31 -0300

    update up.sh

--- fedora/up.sh
+++ fedora/up.sh
@@ -3,10 +3,6 @@
 #Atualização de sistema
 sudo dnf update
 sudo flatpak update
-sudo snap refresh
 
-sudo dnf autoremove
-
-echo ""
-echo "TUDO ATUALIZADO COM SUCESSO!"
-echo ""
+#sudo snap refresh
+#sudo dnf autoremove
